b18e303eaeb012f1ab67e959c6002980de8c3821
SangNguyen1407 <50044154+SangNguyen1407@users.noreply.github.com>
2019-05-14 01:54:40 +0900

    Add files via upload

--- json_parser.c
+++ json_parser.c
@@ -23,10 +23,11 @@ typedef struct JSON_OBJECT
 	char value[JSON_OBJECT_SIZE];
 }JSON_OBJECT;
 
-void skip_whitespace(const char** cursor)
+typedef struct JSON_OBJECT_CONTENT
 {
-
-}
+	int size;
+	JSON_OBJECT *json_obj;
+}JSON_OBJECT_CONTENT;
 
 int get_file_size(char *filename, char *mode)
 {
@@ -85,60 +86,41 @@ int json_parse(JSON_CONTENT json_buff, JSON_OBJECT *json_object) {
 	int c_json = 0;
 	int c_flag = 0;
 	int c_obj_num = 0;
-	int key_flag = 1;
+	int key_flag = 1; /*check key or value*/
 	int c_num	= 0;
-	
+
 	if (json_object == NULL){
 		return 0;
 	}
-	
 	for (; c_json< json_buff.size; c_json++){
-		
-		if (json_buff.buffer[c_json] == '{' || json_buff.buffer[c_json] == '}'||json_buff.buffer[c_json] == '\r' || json_buff.buffer[c_json] == '\n')
-			continue;
-		
-		if (json_buff.buffer[c_json] == ':'){
-			if (key_flag){
-				json_object[c_obj_num].key[c_num++] = '\0';
-			}
-			else{
-				json_object[c_obj_num].value[c_num++] = '\0';
-			}
-			
-			key_flag = 0;
-			c_num = 0;
-			continue;
-		}		
-
-		if (json_buff.buffer[c_json] == '\"' ){
-			continue;
-		}
-
-		if (json_buff.buffer[c_json] == ',' ){
-			if (key_flag){
-				json_object[c_obj_num].key[c_num++] = '\0';
-			}
-			else{
-				json_object[c_obj_num].value[c_num++] = '\0';
-			}
-			
-			c_obj_num ++;
-			c_num = 0;
-			key_flag = 1;
-			continue;
-		}
-		// if (c_flag > 0){
-		if (1){
-			if (key_flag){
-				json_object[c_obj_num].key[c_num++] = json_buff.buffer[c_json];
-			}
-			else{
-				json_object[c_obj_num].value[c_num++] = json_buff.buffer[c_json];
-			}
+		switch (json_buff.buffer[c_json])
+		{
+			case '{':
+			case '}':
+			case '\r':
+			case '\n':
+			case '\"':
+				break;
+			case ':':	/*get value*/
+				key_flag = 0;
+				c_num = 0;
+				break;
+			case ',':	/*the other object*/
+				key_flag = 1; /*get key*/
+				c_num = 0;
+				c_obj_num ++;
+				break;
+			default:
+				if (key_flag){
+					json_object[c_obj_num].key[c_num++] = json_buff.buffer[c_json];
+				}
+				else{
+					json_object[c_obj_num].value[c_num++] = json_buff.buffer[c_json];
+				}
 		}
 			
 	}
-	return 1;
+	return c_obj_num;
 }
 
 int json_changeValue(JSON_OBJECT *json_object, char *key, char *new_value){
@@ -151,46 +133,48 @@ int json_changeValue(JSON_OBJECT *json_object, char *key, char *new_value){
 	for (; json_num < 10; json_num ++){
 		if (strncmp(json_object[json_num].key, key, strlen(key)) == 0){
 			strcpy(json_object[json_num].value, new_value);
-			break;
 		}
 	}
 
 	return 1;
 }
 
-int j2str(JSON_OBJECT *json_object, JSON_CONTENT *json_buff){
+int j2str(JSON_OBJECT_CONTENT json_object, JSON_CONTENT *json_buff){
 	
 	int json_num = 0;
 	int json_buff_end = 0;
-	if (json_object == NULL){
+	if (json_object.json_obj == NULL){
 		return 0;
 	}
-	
-	json_buff->buffer = (char*) malloc(sizeof(char) * (100 + 1) );
+
+	json_buff->buffer = (char*) malloc(sizeof(char) * 200 );
 	json_buff->buffer[0] = '{';
 	json_buff->buffer[1] = '\r';
 	json_buff->buffer[2] = '\n';
 
 	json_buff_end = 3;
-	for (; json_num < 3; json_num ++){
-		
+	for (; json_num < json_object.size; json_num ++){
+		if (json_num != 0){
+			strcpy(json_buff->buffer+(json_buff_end++), ",");
+			json_buff->buffer[json_buff_end++]= '\r';
+			json_buff->buffer[json_buff_end++] = '\n';		
+		}
 		strcpy(json_buff->buffer+(json_buff_end++), "\"");
-		strcpy(json_buff->buffer+json_buff_end, json_object[json_num].key);
-		json_buff_end += strlen(json_object[json_num].key);
+		strcpy(json_buff->buffer+json_buff_end, json_object.json_obj[json_num].key);
+		json_buff_end += strlen(json_object.json_obj[json_num].key);
 		strcpy(json_buff->buffer+(json_buff_end++), "\"");
 		
 		strcpy(json_buff->buffer+(json_buff_end++), ":");
 		
 		strcpy(json_buff->buffer+(json_buff_end++), "\"");
-		strcpy(json_buff->buffer+json_buff_end, json_object[json_num].value);
-		json_buff_end += strlen(json_object[json_num].value);
+		strcpy(json_buff->buffer+json_buff_end, json_object.json_obj[json_num].value);
+		json_buff_end += strlen(json_object.json_obj[json_num].value);
 		strcpy(json_buff->buffer+(json_buff_end++), "\"");
 		
-		strcpy(json_buff->buffer+(json_buff_end++), ",");
-		json_buff->buffer[json_buff_end++]= '\r';
-		json_buff->buffer[json_buff_end++] = '\n';
 		
 	}
+	json_buff->buffer[json_buff_end++]= '\r';
+	json_buff->buffer[json_buff_end++] = '\n';	
 	json_buff->buffer[json_buff_end++] = '}';
 	json_buff->buffer[json_buff_end++] = '\0';
 	
@@ -203,12 +187,13 @@ int j2str(JSON_OBJECT *json_object, JSON_CONTENT *json_buff){
 void main () {
 	JSON_CONTENT json_buff;
 	JSON_CONTENT json_buff_temp;
-	JSON_OBJECT json_object[50];
+	JSON_OBJECT_CONTENT json_object;
 	
 	json_buff = readFile("json.txt", READ_ONLY_MODE);
-	
-	json_parse(json_buff, json_object);
-	json_changeValue(json_object, "tuoi", "30");
+	json_object.size = 7;
+	json_object.json_obj = (JSON_OBJECT *)malloc(sizeof(JSON_OBJECT) * json_object.size);
+	json_object.size = json_parse(json_buff, json_object.json_obj);
+	json_changeValue(json_object.json_obj, "tuoi", "30");
 
 	j2str(json_object, &json_buff_temp);
 	writeFile(json_buff_temp, "json_wr.txt", WRITE_ONLY_MODE);
